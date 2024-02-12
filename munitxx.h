6f9d4ac320ea2db1f41da93a05d8041e83e049e0
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2024-02-12 09:44:05 +0900

    Define munit_assert_stdsv_equal for C++ >= 17

--- munitxx.h
+++ munitxx.h
@@ -29,7 +29,9 @@
 #include <munit.h>
 
 #include <string>
-#include <string_view>
+#if __cplusplus >= 201703L
+#  include <string_view>
+#endif // __cplusplus >= 201703L
 
 #define munit_assert_stdstring_equal(a, b)                                     \
   do {                                                                         \
@@ -44,24 +46,29 @@
     MUNIT_PUSH_DISABLE_MSVC_C4127_                                             \
   } while (0) MUNIT_POP_DISABLE_MSVC_C4127_
 
-#define munit_assert_stdsv_equal(a, b)                                         \
-  do {                                                                         \
-    const std::string_view munit_tmp_a_ = a;                                   \
-    const std::string_view munit_tmp_b_ = b;                                   \
-    if (MUNIT_UNLIKELY(a != b)) {                                              \
-      munit_hexdump_diff(stderr, munit_tmp_a_.data(), munit_tmp_a_.size(),     \
-                         munit_tmp_b_.data(), munit_tmp_b_.size());            \
-      munit_errorf("assertion failed: string %s == %s (\"%.*s\" == \"%.*s\")", \
-                   #a, #b, (int)munit_tmp_a_.size(), munit_tmp_a_.data(),      \
-                   (int)munit_tmp_b_.size(), munit_tmp_b_.data());             \
-    }                                                                          \
-    MUNIT_PUSH_DISABLE_MSVC_C4127_                                             \
-  } while (0) MUNIT_POP_DISABLE_MSVC_C4127_
+#if __cplusplus >= 201703L
+#  define munit_assert_stdsv_equal(a, b)                                       \
+    do {                                                                       \
+      const std::string_view munit_tmp_a_ = a;                                 \
+      const std::string_view munit_tmp_b_ = b;                                 \
+      if (MUNIT_UNLIKELY(a != b)) {                                            \
+        munit_hexdump_diff(stderr, munit_tmp_a_.data(), munit_tmp_a_.size(),   \
+                           munit_tmp_b_.data(), munit_tmp_b_.size());          \
+        munit_errorf(                                                          \
+            "assertion failed: string %s == %s (\"%.*s\" == \"%.*s\")", #a,    \
+            #b, (int)munit_tmp_a_.size(), munit_tmp_a_.data(),                 \
+            (int)munit_tmp_b_.size(), munit_tmp_b_.data());                    \
+      }                                                                        \
+      MUNIT_PUSH_DISABLE_MSVC_C4127_                                           \
+    } while (0) MUNIT_POP_DISABLE_MSVC_C4127_
+#endif // __cplusplus >= 201703L
 
 #if defined(MUNIT_ENABLE_ASSERT_ALIASES)
 
 #  define assert_stdstring_equal(a, b) munit_assert_stdstring_equal(a, b)
-#  define assert_stdsv_equal(a, b) munit_assert_stdsv_equal(a, b)
+#  if __cplusplus >= 201703L
+#    define assert_stdsv_equal(a, b) munit_assert_stdsv_equal(a, b)
+#  endif // __cplusplus >= 201703L
 
 #endif /* defined(MUNIT_ENABLE_ASSERT_ALIASES) */
 
